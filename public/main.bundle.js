7f49b4e9016c004a8f642fcc2dcd2e0b193bbdf8
Muskan Shrestha <musk0047@edu.easj.dk>
2017-12-30 22:50:40 +0100

    updates

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -1246,8 +1246,8 @@ var AuthService = (function () {
     function AuthService(http, httpc) {
         this.http = http;
         this.httpc = httpc;
-        this.localhost = 'http://localhost:8080/';
-        // this.localhost = '';
+        // this.localhost = 'http://localhost:8080/';
+        this.localhost = '';
     }
     AuthService.prototype.registerUser = function (user) {
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
